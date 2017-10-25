d682ea7c6b68145eea6023fb1e62ba745e6a221a
Don Kelly <karfai@gmail.com>
2017-10-25 11:09:10 +0400

    Adjustments to the image build and ignore
    Change-Id: If134e3588f07c8fa5b09c97186b961e9a9dddede

--- docker/ruby-base/publish.sh
+++ docker/ruby-base/publish.sh
@@ -3,7 +3,7 @@ VERSION=`cat .version`
 ENVIRONMENTS="development production"
 
 for en in $ENVIRONMENTS; do
-    docker build -t "xalgorithms/xadf-base-image-ruby:latest-$en" -t "xalgorithms/xadf-base-image-ruby:$VERSION-$en" -f "Dockerfile.$en" .
+    docker build --squash -t "xalgorithms/xadf-base-image-ruby:latest-$en" -t "xalgorithms/xadf-base-image-ruby:$VERSION-$en" -f "Dockerfile.$en" .
     docker push "xalgorithms/xadf-base-image-ruby:latest-$en"
     docker push "xalgorithms/xadf-base-image-ruby:$VERSION-$en"
 done
